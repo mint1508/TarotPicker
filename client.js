72f216ed052998fb76f8589d78fb365ff73991c1
Minh Lê <minhle1508@gmail.com>
2023-09-02 23:01:47 +0700

    Create client.js

---
+++ client.js
@@ -0,0 +1,110 @@
+function pickRandomCards(numCards) {
+    const allCards = [
+        // Major Arcana (22 cards)
+        "The Fool",
+        "The Magician",
+        "The High Priestess",
+        "The Empress",
+        "The Emperor",
+        "The Hierophant",
+        "The Lovers",
+        "The Chariot",
+        "Strength",
+        "The Hermit",
+        "Wheel of Fortune",
+        "Justice",
+        "The Hanged Man",
+        "Death",
+        "Temperance",
+        "The Devil",
+        "The Tower",
+        "The Star",
+        "The Moon",
+        "The Sun",
+        "Judgment",
+        "The World",
+
+        // Minor Arcana - Wands (14 cards)
+        "Ace of Wands",
+        "Two of Wands",
+        "Three of Wands",
+        "Four of Wands",
+        "Five of Wands",
+        "Six of Wands",
+        "Seven of Wands",
+        "Eight of Wands",
+        "Nine of Wands",
+        "Ten of Wands",
+        "Page of Wands",
+        "Knight of Wands",
+        "Queen of Wands",
+        "King of Wands",
+
+        // Minor Arcana - Cups (14 cards)
+        "Ace of Cups",
+        "Two of Cups",
+        "Three of Cups",
+        "Four of Cups",
+        "Five of Cups",
+        "Six of Cups",
+        "Seven of Cups",
+        "Eight of Cups",
+        "Nine of Cups",
+        "Ten of Cups",
+        "Page of Cups",
+        "Knight of Cups",
+        "Queen of Cups",
+        "King of Cups",
+
+        // Minor Arcana - Swords (14 cards)
+        "Ace of Swords",
+        "Two of Swords",
+        "Three of Swords",
+        "Four of Swords",
+        "Five of Swords",
+        "Six of Swords",
+        "Seven of Swords",
+        "Eight of Swords",
+        "Nine of Swords",
+        "Ten of Swords",
+        "Page of Swords",
+        "Knight of Swords",
+        "Queen of Swords",
+        "King of Swords",
+
+        // Minor Arcana - Pentacles (14 cards)
+        "Ace of Pentacles",
+        "Two of Pentacles",
+        "Three of Pentacles",
+        "Four of Pentacles",
+        "Five of Pentacles",
+        "Six of Pentacles",
+        "Seven of Pentacles",
+        "Eight of Pentacles",
+        "Nine of Pentacles",
+        "Ten of Pentacles",
+        "Page of Pentacles",
+        "Knight of Pentacles",
+        "Queen of Pentacles",
+        "King of Pentacles"
+    ];
+    const shuffledCards = allCards.slice(); // Create a copy of the array
+
+    // Fisher-Yates shuffle algorithm
+    for (let i = shuffledCards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledCards[i], shuffledCards[j]] = [shuffledCards[j], shuffledCards[i]];
+    }
+
+    const selectedCards = shuffledCards.slice(0, numCards);
+
+    return selectedCards;
+
+}
+const btn = document.querySelector('#btnSubmit');
+btn.addEventListener("click",function(e){
+   //your handler here
+  const numberCardToPick = document.querySelector('#numCard')?.value || 3;
+  const randomCards = pickRandomCards(numCardsToPick);
+  document.querySelector('#result').value = randomCards.join('\n');
+},false);
